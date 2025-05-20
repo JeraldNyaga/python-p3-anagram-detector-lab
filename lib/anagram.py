b808afc880c823403ee6a2addaf62bf521800c63
judgeOstrich <jeraldnyaga15@gmail.com>
2025-05-20 19:52:04 +0300

    Anagram method

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, list_of_words: list):
+        matches = []
+
+        for word in list_of_words:
+            flag = True
+            counter = 0
+            if len(word) != len(self.word):
+                continue
+            else:
+                while flag and counter < len(self.word):
+                    if word.count(word[counter]) != self.word.count(self.word[counter]):
+                        flag
+                        flag = False
+                    else:
+                        counter += 1
+            matches.append(word) if flag else None
+        return matches
